3b2b7a7bbad7941a4f5d8aaa293ce62980588914
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2015-09-14 20:40:57 -0500

    ejercicios 24-08

---
+++ verificar numero en lista.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Ingrese el numero que desea encontrar "<< endl ;
+int a;
+cin>>a;
+
+	int vector[a];
+vector[1]=42;
+vector[2]=4;
+vector[3]=7;
+	if (a=vector[3]){
+		cout<<"nucorrecto"<<endl;
+	}
+	
+
+
+
+}
