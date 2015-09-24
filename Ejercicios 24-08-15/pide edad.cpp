3b2f0b9f1379a9118c3bd2916b76e37c7e46936f
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2015-09-23 22:40:12 -0500

    Ejercicios 24-08-15

---
+++ Ejercicios 24-08-15/pide edad.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+	int a; 
+	std:: cout << " Edad: ";
+	std:: cin>> a;
+	if(a > 20){
+		std:: cout << " estas viejo "<<"\n";
+	} else {
+		std:: cout << " estas joven "<<"\n";
+	}
+	return 0;
+}
+
---
+++ Ejercicios 24-08-15/suma de dos numeros.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+	int a; 
+	std:: cin>> a;
+	int b;
+	std:: cin>> b;
+	int sum;
+	sum = a + b;
+	std:: cout << " the sum of "<< a << " and " << b << " is " << sum <<"\n";
+	return 0;
+}
+
